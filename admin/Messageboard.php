39f83a869d55dcbede7575629da0f98853cf77dd
Donald B Kennedy <DonaldB.Kennedy@gmail.com>
2014-03-18 04:28:14 -0400

    Have the form for posting message board messages, have the function to add messages to the database set up, just need to make it work on click. Made a class for the message board messages for retrieval as well.

--- admin/Messageboard.php
+++ admin/Messageboard.php
@@ -1,21 +1,38 @@
+<html>
 <?php
 include 'main.php';
+//Auto loads all the class files in the classes folder
+//Use require_once "dirname(dirname(__FILE__)) ." without quotes in front of '\AutoLoader.php' if you need to go up 2 directories to root. "dirname(__FILE__) ." for 1 directory up.
+require_once dirname(dirname(__FILE__)) . '\AutoLoader.php';
+spl_autoload_register(array('AutoLoader', 'autoLoad'));
+if(!isset($_SESSION)){
+	session_start();
+}
+if(!(empty($_SESSION)))
+{
+	/*if($_SESSION['sess_role'] != 1)
+	{
+		header('Refresh: 1.5; url=../index.php');
+		echo '<link href="../bootstrap/css/confirmationAccount.css" rel="stylesheet">';
+		exit('<html><body style="background-color: white; font-size: 20px; font-weight: bold; color: black;"><div class="form-wrapper" 
+		style="text-align: center; vertical-align: middle"><p>You do not have the correct privileges to access this page.</p></div></body></html>');
+	}*/
+}
+else
+{
+	header('Location: ../index.php');
+}
+$layout = new Layout();
+$database = new PDO('mysql:host=localhost;dbname=sms;charset=utf8', 'root', '');
+$session = new Session($_SESSION, $database);
+?>
 
-echo '<tr>';
-    echo '<td class="leftpart">';
-        echo '<h3>Message Board : Welcome to The Message Board<br />
-		
-		</a></h3>';
-    echo '</td>';
- 
-    echo '</td>';
-echo '</tr>';
 
-if (isset($_POST['Message'])) $Message = $_POST['Message'];
-else $Message = "(Posted on 02/10/2014)";
-echo <<<_END
-<html>
-	<head>
+<!-- Custom styles for this template -->
+<link rel="stylesheet" href="bootstrap/css/jquery-ui-1.10.4.custom.css" type="text/css" /> 
+
+
+<head>
 	<title>Admin's Message Board</title>
 	</head>
 	<body>
@@ -26,7 +43,31 @@ echo <<<_END
 		<input type = "Post" />
 		</form>
 		</body>
-</html>
-_END;
 
-?>
+
+<div class="container jumbotron">
+	<br />
+	<form name="compose" id="compose-form" action="#" method="post">
+		<pre><textarea id="message" name="message" class="emailMessage"></textarea></pre>
+		<button class="btn btn-success btn-xs" onclick="postMsg()">Submit Post</button>
+	</form>
+</div>
+
+<script src="bootstrap/js/jquery-ui-1.10.4.custom.js"></script>	
+<script src="bootstrap/js/compose.js"></script>
+<script>
+function postMsg()
+{
+	$.post(
+		'admin/postMessage.php',
+		{
+			'message' : $('#message').val()
+		},
+		function(data){
+		  $("#mainDiv").html(data);
+		}
+	  );
+  return false;
+}
+</script>
+</html>
