fec59a17c4db940ebd31901476deb69c73055402
Steven Ng <stevenng308@gmail.com>
2014-04-01 21:45:24 -0400

    Allowing money format inputs for adding lunch money. Will not add amounts past 999999.99.

--- bootstrap/js/addMoney.js
+++ bootstrap/js/addMoney.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 	var current = new Date();
+	var month = current.getMonth() + 1;
 	var year = current.getFullYear();
 	$.validator.addMethod("noSpecial", 
         function(value, element, regexp) {
@@ -28,12 +29,48 @@ $(document).ready(function () {
 		"No numbers or special characters allowed"
 	);
 	
+	//rule for allowing up to 2 decimal places
+	$.validator.addMethod("twoDecimals", 
+        function(value, element, regexp) {
+			var regex = new RegExp(/^(?!0\.00)[1-9](\d*\.\d{1,2}|\d+)$/);
+			var key = value;
+			
+			if (!regex.test(key)) {
+			   return false;
+			}
+			return true;
+		},
+		"Invalid format. Please format your input like the example shown (ex. 1000.75)"
+	);
+	
+	//rule for checking the expiration info
+	$.validator.addMethod("checkExpire", 
+        function(value, element, regexp) {
+			if (value == year)
+			{
+				if ($('#monthExpiration').val() >= month)
+					return true;
+				else
+					return false;
+			}
+			else
+			{
+				if (value > year)
+					return true;
+				else
+					return false;
+			}
+		},
+		"Card has expired"
+	);
+	
 	$('#addBalance-form').validate({
         rules: {
 			addingBalance: {
+				//digits: true,
 				//number: true,
-				digits: true,
 				range: [1, 100000],
+				twoDecimals: true,
 				required: true
 			},
 			cardHolderName: {
@@ -52,13 +89,18 @@ $(document).ready(function () {
 			yearExpiration: {
 				digits: true,
 				range: [year, 2100],
+				checkExpire: true,
 				required: true
 			}
 		},
 		messages: {
 			addingBalance: {
 				//number: "Please enter a valid amount"
-				digits: "Please enter a valid whole amount greater than 0"
+				digits: "Please enter a valid whole amount greater than 0. No decimals."
+			},
+			monthExpiration: {
+				//number: "Please enter a valid amount"
+				range: "Please enter a valid whole amount greater than 0. No decimals."
 			}
 		},
         highlight: function (element) {
@@ -95,5 +137,10 @@ $(document).ready(function () {
 				return false;
 			}
 		});
-	});                   
+	});
+
+	$('#monthExpiration').change().keyup(function () {
+		$('#yearExpiration').val('');
+		$('#yearExpiration').valid();
+	});
 });
--- classes/addMoney.php
+++ classes/addMoney.php
@@ -20,20 +20,28 @@ $session = new Session($_SESSION, $database);
 $student_id = mysql_real_escape_string($_POST['studentID']);
 $adding_balance = mysql_real_escape_string($_POST['addingBalance']);
 
-//$query = "UPDATE student SET balance = balance + '" . $adding_balance . "' WHERE studentID = '" . $student_id . "'";
-//$stmt = $database->query($query);
-//$result = $stmt->fetch();
-
-$database->exec("UPDATE student SET balance = balance + '" . $adding_balance . "' WHERE studentID = " . $student_id . "");
+$query = $database->query("SELECT balance FROM student WHERE studentID = '" . $student_id . "'");
+$result = $query->fetchAll(PDO::FETCH_ASSOC);
+$bal = $result[0]['balance'];
+$bal += $adding_balance;
+if ($bal > 1000000)
+{
+	$confirm = 'Amount not added. The child\'s balance will exceed $1,000,000.<br />Please consider donating the excess money to a charitable cause (i.e. Me).';
+}
+else
+{
+	$confirm = 'Amount has been added.';
+	$database->exec("UPDATE student SET balance = balance + '" . $adding_balance . "' WHERE studentID = " . $student_id . "");
+}
 ?>
 <!-- Custom styles for this template -->
 <link href="bootstrap/css/confirmationAccount.css" rel="stylesheet">
 
 <div class="container jumbo-tron form-wrapper" style="text-align:center; vertical-align:middle">
 	<div class="jumbotron">
-		<form class="form-signin" style="text-valign:center">
-			<h3>Amount has been added.</h3>
+		<div class="container" style="text-valign:center">
+			<h3><?php echo $confirm; ?></h3>
 			<a class="btn btn-primary" href="<?php echo $session->getUserTypeFormatted(); ?>/main.php" role="button">Return Home</a>
-		</form>
+		</div>
 	</div>
 </div>
